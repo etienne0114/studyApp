251cbadc8ab4d58456f8b3c706a146a1ea0119bf
etienne0114 <etiennetuyihamye@gmail.com>
2025-04-23 20:33:57 +0200

    errors 3

--- lib/data/models/activity.dart
+++ lib/data/models/activity.dart
@@ -81,7 +81,7 @@ class Activity {
       type: type ?? this.type,
       startTime: startTime ?? this.startTime,
       endTime: endTime ?? this.endTime,
-      isCompleted: isCompleted ?? this._isCompleted,
+      isCompleted: isCompleted ?? _isCompleted,
       notificationEnabled: notificationEnabled ?? this.notificationEnabled,
       notificationMinutesBefore: notificationMinutesBefore ?? this.notificationMinutesBefore,
       location: location ?? this.location,
